b440ed6453363724ea005c35b83ad61a89e3f0c2
aldo <aldo@numherit.com>
2023-02-15 01:20:36 +0300

    feat: add feature crud front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { extract } from './common/utils';
+import { UserService } from './services/user/user.service';
+import { IResponse } from './shared/interfaces/IResponse';
+import { User } from './shared/models/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  modalRef?: BsModalRef;
+  constructor(private userService: UserService, private modalService: BsModalService){}
   title = 'crud-user-front';
+  public userList: User[]  = [];
+  public user: User = {} as User;
+  public titleModal = '';
+  public isAdd: boolean = false;
+  ngOnInit(): void {
+    this.userService.getAllUser().subscribe(
+      ({ data }) => {
+        this.userList =  data as User[]
+      }
+    )
+  }
+
+ 
+  openAddModal(template: TemplateRef<any>, btn: TemplateRef<any>  | any, user?: User) {
+    if(user) {
+      this.isAdd = false;
+      this.titleModal = 'Edit user';
+      this.user = user;
+    } else {
+      this.isAdd = true;
+      this.titleModal = 'Add user';
+    }
+    this.modalRef = this.modalService.show(template);
+  }
+
+  addUser(btn: TemplateRef<HTMLButtonElement>  | any) {
+    btn.disabled = true;
+    if(this.isAdd) {
+      this.userService.createUser(this.user).subscribe(
+        ({ data }) => {
+          this.userList =  [...this.userList, data as User];
+          btn.disabled = false;
+          this.closeModal()
+        }
+      )
+    } else {
+      const rest = extract(this.user,'_id','__v')
+      this.userService.updateUserById(this.user,rest._id).subscribe(
+        ({ data }) => {
+          //this.userList =  [...this.userList, data as User];
+          btn.disabled = false;
+          this.closeModal()
+        }
+      )
+    }
+  }
+  deleteUser(id: String = '') {
+    this.userService.deleteUerById(id).subscribe(
+      ({ data }) => {
+        this.userList =  this.userList.filter((user: User) => user._id !== id);
+      }
+    )
+  }
+  
+  openDetailUser(template: TemplateRef<any>,id: string = 'open') {
+    this.userService.getUserById(id).subscribe(
+      ({ data }) => {
+        this.user = data as User;
+      }
+    )
+    this.modalRef = this.modalService.show(template);
+  }
+  closeModal() {
+    this.modalRef?.hide();
+    this.user = {} as User;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { DataTablesModule } from "angular-datatables";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +18,10 @@ import { HeaderComponent } from './shared/components/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DataTablesModule
+    DataTablesModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
